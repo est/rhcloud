ff95f2d8a507bb6240f06a88779151814c4498a3
est <electronicstar@126.com>
2013-02-19 15:12:24 +0800

    test domains

--- wsgi/main.py
+++ wsgi/main.py
@@ -40,14 +40,16 @@ def index(query=''):
     return template('index.html', query=query.decode('utf8', 'replace'), req=request.query)
 
 tools_app = Bottle()
+tools_app.hostnames = ['t.est.im']
 
-@tools_app.route(('t.est.im', '/ip'))
+@tools_app.route('/ip')
 def show_ip():
-    return ('1')
+    return request.environ.get('REMOTE_ADDR', '')
 
-@tools_app.route('/s/ip')
-def show_ip2():
-    return ('2')
+@tools_app.route('/ua')
+def show_ua():
+    return request.environ.get('HTTP_USER_AGENT', '')
+
 
 # @ToDo: rewrire http://bottlepy.org/docs/dev/_modules/bottle.html
 # http://bottlepy.org/docs/dev/api.html
@@ -70,14 +72,18 @@ def application(environ, start_response):
             return app(environ, start_response)
     return default_app()(environ, start_response)
 
+
 if '__main__' == __name__:
     try:
         import readline, rlcompleter; readline.parse_and_bind("tab: complete")
     except:
         pass
 
-    DEV_APP = dict_app
-    DEV_APP.hostnames.append('10.0.18.3:8002')
+    DEV_APP = tools_app # dict_app
+    if getattr(DEV_APP, 'hostnames', None):
+        DEV_APP.hostnames.append('10.0.18.3:8002')
+    else:
+        DEV_APP.hostnames = ['10.0.18.3:8002']
     DEV_APP.mount('/static', static_app)
 
     __import__('BaseHTTPServer').BaseHTTPRequestHandler.address_string = lambda x:x.client_address[0]
