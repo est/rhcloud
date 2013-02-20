fb9efede4d06322aafde11c9d73b70918575ff61
est <electronicstar@126.com>
2013-02-20 14:20:06 +0800

    deploy script test

---
+++ wsgi/vps_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+access_token=
+if [ -z $1 ]; then head=master; else head=$1; fi
+repo_url="https://github.com/est/rhcloud/tarball/$head"
+t_url="https://api.github.com/repos/est/est/tarball/$head"
+www_dir="/home/est/sites/rhcloud"
+
+#wget --no-check-certificate -Sq -O- $repo_url | tar -C $www_dir --strip=1 -zmxf -
+# rev=`ls -d PREFIX-$repo_name* | awk '{print substr($1,length($1)-39)}'`
+
+
+
+rev=$(wget -Sq -O- $t_url 2> >( 
+    grep "Content-Disposition:" | 
+    tail -1 | 
+    awk 'match($0, /filename=.+\-([a-zA-Z0-9]+)\./, f){ print f[1] }'
+    ) > >(
+    tar -C $www_dir --strip=1 -zmxf -
+    ))
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo `date --rfc-3339=s`,$rev,$SSH_CONNECTION >> $DIR\var\deploy.oog
