54c6a4c9230873e6113457dac8305e1e27001835
est <electronicstar@126.com>
2013-03-18 15:11:02 +0800

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='est projects',
+      version='1.0',
+      description='def and tools collection',
+      author='est',
+      author_email='electronicstar@126.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['bottle', 'flask'],
+     )
