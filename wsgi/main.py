29b2ebc228815a07613ec6bd9a5c76edeb69ccde
est <electronicstar@126.com>
2013-09-09 16:47:14 +0800

    change to sqlite

--- wsgi/main.py
+++ wsgi/main.py
@@ -17,13 +17,13 @@ rel_path = lambda x: os.path.join(os.path.realpath(os.path.dirname(__file__)), x
 bottle.TEMPLATE_PATH.append(rel_path('templates'))
 STATIC_ROOT = rel_path('static')
 
-# db = peewee.SqliteDatabase(rel_path('db.sqlite3'))
-db = peewee.MySQLDatabase('backend', 
-    host=os.environ.get('OPENSHIFT_MYSQL_DB_HOST', '127.0.0.1'), 
-    port=int(os.environ.get('OPENSHIFT_MYSQL_DB_PORT', '3306')),
-    user='bu',
-    passwd='bupassword@',
-    )
+db = peewee.SqliteDatabase(rel_path('db.sqlite3'))
+# db = peewee.MySQLDatabase('backend', 
+#     host=os.environ.get('OPENSHIFT_MYSQL_DB_HOST', '127.0.0.1'), 
+#     port=int(os.environ.get('OPENSHIFT_MYSQL_DB_PORT', '3306')),
+#     user='bu',
+#     passwd='bupassword@',
+#     )
 
 # handle connect timeout issues
 # poor-man's connection pool
