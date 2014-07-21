e816fbe432089669bfbe0c396eff5d91d2626342
est <electronicstar@126.com>
2014-07-21 23:12:11 +0800

    my own srardict parse module

---
+++ wsgi/est_startdict.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# coding: utf8
+
+# http://code.google.com/p/babiloo/wiki/StarDict_format
+
+import struct, gzip
+import glob, os
+
+class StarDict(object):
+    def __init__(self, dir_prefix=''):
+        self.word_index = {}
+        self.idx_filename = glob.glob(dir_prefix+'.idx')[0]
+        self.parse_idx_file(self.idx_filename)
+
+        self.dic_filename = glob.glob(dir_prefix+'.dict*')[0]
+        if self.dic_filename.endswith('.dz'):
+            self.dic_file = gzip.open(self.dic_filename, 'rb')
+        else:
+            self.dic_file = open(self.dic_filename, 'rb')
+
+    def parse_idx_file(self, filename):
+        l = os.stat(filename).st_size
+        data = bytearray(l)
+        f = open(filename, 'rb')
+        f.readinto(data)
+        f.close()
+        i = 0
+        while True:
+            o = data.find('\0', i)
+            k = str(data[i:o])
+            v = struct.unpack_from('!II', buffer(data), o+1)
+            self.word_index[ k ] = v
+            i = o + 1 + 8 # 1 byte for 0x00 and 8 bytes for (offset, length)
+            if i>= l:
+                break
+
+    def get_def(self, word=''):
+        pass
+
+
+if '__main__' == __name__:
+    d = StarDict('def/stardict-dictd-web1913-2.4.2/dictd_www.dict.org_web1913')
